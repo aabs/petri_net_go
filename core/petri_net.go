a747ac80a639cce7cfdcfb7c755a6a0f31192fb9
Andrew Matthews <amatthews2@agl.com.au>
2022-03-22 11:16:31 +1100

    some builder tests

--- core/petri_net.go
+++ core/petri_net.go
@@ -3,8 +3,6 @@
 package core
 
 import (
-	"math/rand"
-
 	"gonum.org/v1/gonum/mat"
 )
 
@@ -19,17 +17,201 @@ type PetriNet struct {
 	TransitionNames          map[int]string
 }
 
-func NewPetriNet(number_of_places, number_of_transitions int) (*PetriNet, error) {
-	data := make([]float64, number_of_places*number_of_transitions)
-	for i := range data {
-		data[i] = rand.NormFloat64()
+/*
+ * Building Places
+ */
+var placeIdGenerator int = 0
+
+type Place struct {
+	Id   int
+	Name string
+}
+
+type PlaceBuilder struct {
+	Id   int
+	Name string
+}
+
+func CreatePlace() *PlaceBuilder {
+	placeIdGenerator++
+	return &PlaceBuilder{
+		Id: placeIdGenerator,
 	}
+}
+
+func (p *PlaceBuilder) Called(name string) *PlaceBuilder {
+	p.Name = name
+	return p
+}
+
+func (p *PlaceBuilder) Build() *Place {
+	return &Place{
+		Id:   p.Id,
+		Name: p.Name,
+	}
+}
+
+type Pair[T1 any, T2 any] struct {
+	Item1 T1
+	Item2 T2
+}
+
+func NewPair[T1 any, T2 any](item1 T1, item2 T2) *Pair[T1, T2] {
+	return &Pair[T1, T2]{
+		Item1: item1,
+		Item2: item2,
+	}
+}
+
+/*
+ * Building Transitions
+ */
+var transIdGenerator int = 0
+
+type Transition struct {
+	Id   int
+	Name string
+}
+
+type TransitionBuilder struct {
+	Id   int
+	Name string
+}
+
+func CreateTransition() *TransitionBuilder {
+	transIdGenerator++
+	return &TransitionBuilder{
+		Id: transIdGenerator,
+	}
+}
+
+func (p *TransitionBuilder) Called(name string) *TransitionBuilder {
+	p.Name = name
+	return p
+}
+
+func (p *TransitionBuilder) Build() *Transition {
+	return &Transition{
+		Id:   p.Id,
+		Name: p.Name,
+	}
+}
+
+/*
+ * Building Petri Nets
+ */
+
+type PetriNetBuilder struct {
+	Name                     string
+	Version                  string
+	InputIncidence           []Pair[int, int]
+	OutputIncidence          []Pair[int, int]
+	InhibitoryInputIncidence []Pair[int, int]
+	Places                   []Place
+	Transitions              []Transition
+}
+
+func CreatePetriNet() *PetriNetBuilder {
+	return &PetriNetBuilder{
+		Places:      []Place{},
+		Transitions: []Transition{},
+	}
+}
+
+func (p *PetriNetBuilder) Called(name string) *PetriNetBuilder {
+	p.Name = name
+	return p
+}
+
+func (p *PetriNetBuilder) WithVersion(version string) *PetriNetBuilder {
+	p.Version = version
+	return p
+}
+
+func (p *PetriNetBuilder) WithPlace(pl Place) *PetriNetBuilder {
+	p.Places = append(p.Places, pl)
+	return p
+}
+func (p *PetriNetBuilder) WithTransition(t Transition) *PetriNetBuilder {
+	p.Transitions = append(p.Transitions, t)
+	return p
+}
+
+func (p *PetriNetBuilder) WithInArc(arc Pair[int, int]) *PetriNetBuilder {
+	p.InputIncidence = append(p.InputIncidence, arc)
+	return p
+}
+
+func (p *PetriNetBuilder) WithOutArc(arc Pair[int, int]) *PetriNetBuilder {
+	p.OutputIncidence = append(p.OutputIncidence, arc)
+	return p
+}
+func (p *PetriNetBuilder) WithInhibitorArc(arc Pair[int, int]) *PetriNetBuilder {
+	p.InhibitoryInputIncidence = append(p.InhibitoryInputIncidence, arc)
+	return p
+}
+
+func (p *PetriNetBuilder) BuildInputIncidenceMatrix() []float64 {
+	number_of_places := len(p.Places)
+	number_of_transitions := len(p.Transitions)
+	result := make([]float64, number_of_places*number_of_transitions)
+	// initialize result with zeros
+	for i := range result {
+		result[i] = 0
+	}
+
+	// set input arcs to 1
+	for _, arc := range p.InputIncidence {
+		result[(arc.Item1-1)*number_of_places+(arc.Item2-1)] = 1
+	}
+
+	return result
+}
+
+func (p *PetriNetBuilder) BuildOutputIncidenceMatrix() []float64 {
+	number_of_places := len(p.Places)
+	number_of_transitions := len(p.Transitions)
+	result := make([]float64, number_of_places*number_of_transitions)
+	// initialize result with zeros
+	for i := range result {
+		result[i] = 0
+	}
+
+	// set input arcs to 1
+	for _, arc := range p.OutputIncidence {
+		result[(arc.Item1-1)*number_of_places+(arc.Item2-1)] = 1
+	}
+
+	return result
+}
+
+func (p *PetriNetBuilder) BuildInhibitoryInputIncidenceMatrix() []float64 {
+	number_of_places := len(p.Places)
+	number_of_transitions := len(p.Transitions)
+	result := make([]float64, number_of_places*number_of_transitions)
+	// initialize result with zeros
+	for i := range result {
+		result[i] = 0
+	}
+
+	// set input arcs to 1
+	for _, arc := range p.InhibitoryInputIncidence {
+		result[(arc.Item1-1)*number_of_places+(arc.Item2-1)] = 1
+	}
+
+	return result
+}
+
+func (p *PetriNetBuilder) Build() (*PetriNet, error) {
+	number_of_places := len(p.Places)
+	number_of_transitions := len(p.Transitions)
+
 	return &PetriNet{
-		Name:                     "",
-		Version:                  "",
-		InputIncidence:           mat.NewDense(number_of_places, number_of_transitions, data),
-		OutputIncidence:          mat.NewDense(number_of_places, number_of_transitions, data),
-		InhibitoryInputIncidence: mat.NewDense(number_of_places, number_of_transitions, data),
+		Name:                     p.Name,
+		Version:                  p.Version,
+		InputIncidence:           mat.NewDense(number_of_places, number_of_transitions, p.BuildInputIncidenceMatrix()),
+		OutputIncidence:          mat.NewDense(number_of_places, number_of_transitions, p.BuildOutputIncidenceMatrix()),
+		InhibitoryInputIncidence: mat.NewDense(number_of_places, number_of_transitions, p.BuildInhibitoryInputIncidenceMatrix()),
 		PlaceNames:               make(map[int]string),
 		TransitionNames:          make(map[int]string),
 	}, nil
--- core/petri_net_test.go
+++ core/petri_net_test.go
@@ -4,10 +4,70 @@ import (
 	"testing"
 )
 
-func Test_petriNet_NewPetriNet(t *testing.T) {
+/*func Test_petriNet_NewPetriNet(t *testing.T) {
 	sut, err := NewPetriNet(5, 5)
 
 	if err != nil {
+		t.Error(sut)
+	}
+}
+*/
+func Test_PlaceBuilder_Create(t *testing.T) {
+	sut := CreatePlace()
+
+	if sut.Id != 1 {
+		t.Error(sut)
+	}
+}
+func Test_PlaceBuilder_Called(t *testing.T) {
+	sut := CreatePlace().
+		Called("Test")
+
+	if sut.Name != "Test" {
+		t.Error(sut)
+	}
+}
+func Test_TransitionBuilder_Create(t *testing.T) {
+	sut := CreateTransition()
+
+	if sut.Id != 1 {
+		t.Error(sut)
+	}
+}
+func Test_TransitionBuilder_Called(t *testing.T) {
+	sut := CreateTransition().
+		Called("Test")
+
+	if sut.Name != "Test" {
+		t.Error(sut)
+	}
+}
+
+
+func Test_PetriNetBuilder_Create(t *testing.T) {
+	sut := CreatePetriNet().
+		Called("PT")
+
+	if sut.Name != "PT" {
+		t.Error(sut)
+	}
+}
+
+func Test_PetriNetBuilder_FullCreate(t *testing.T) {
+	sut, err := CreatePetriNet().
+		Called("PT").
+		WithPlace(*CreatePlace().Called("P1").Build()).
+		WithPlace(*CreatePlace().Called("P2").Build()).
+		WithTransition(*CreateTransition().Called("T1").Build()).
+		WithInArc(*NewPair(1,1)).
+		WithOutArc(*NewPair(1,2)).
+		Build()
+
+	if err != nil {
+		t.Error(sut)
+	}
+	
+	if sut.Name != "PT" {
 		t.Error(sut)
 	}
 }
